0b0f2cd567a280789373f0ddc414c2aca5bd8973
tsubasa_develop <t.ohtsuka@re-tech.net>
2023-05-20 02:32:25 +0900

    remove: build

--- build/_app/immutable/nodes/2.57f533af.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as n,s as o}from"../chunks/index.ccd4248b.js";const s=!0,a="always",i=Object.freeze(Object.defineProperty({__proto__:null,prerender:s,trailingSlash:a},Symbol.toStringTag,{value:"Module"}));class u extends t{constructor(e){super(),n(this,e,null,null,o,{})}}export{u as component,i as universal};
